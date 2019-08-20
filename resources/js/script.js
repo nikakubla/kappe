2a2ae4ad0b3816bef29ce802cf39c75bc44cc50d
Nika <n.kublashvili@webintelligence.de>
2019-08-20 22:11:59 +0400

    change

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,45 +1,38 @@
-
 $(document).ready(function($) {
-	
+
     var toggleInfo = $('.info-toggle'),
-		toggleNavbar = $('.navbar-right');
+        toggleNavbar = $('.navbar-right');
 
-	toggleInfo.on('click', function(e){
-		e.preventDefault();
+    var toggleBurger = $('.burger'),
+        toggleMenu = $('.menu');
 
-		if ( !$(this).hasClass('active') ) {
-			$(this).addClass('active');
-			toggleNavbar.addClass('active');
-		} else {
-			$(this).removeClass('active');
-			toggleNavbar.removeClass('active');				
-		}
-	});
+    toggleInfo.on('click', function(e) {
+        e.preventDefault();
+
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+            toggleNavbar.addClass('active');
+        } else {
+            $(this).removeClass('active');
+            toggleNavbar.removeClass('active');
+        }
+
+    });
+
+    toggleBurger.on('click', function(e) {
+        e.preventDefault();
+
+
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+            toggleMenu.addClass('active');
+
+
+        } else {
+            $(this).removeClass('active');
+            toggleMenu.removeClass('active');
+
+
+        }
+    });
 });
-
-$(document).ready(function($) {
-	
-    var toggleInfo = $('.burger'),
-		toggleNavbar = $('.menu');
-
-	toggleInfo.on('click', function(e){
-		e.preventDefault();
-			
-
-		if ( !$(this).hasClass('active') ) {
-			$(this).addClass('active');
-			toggleNavbar.addClass('active');
-			
-
-		} else {
-			$(this).removeClass('active');
-			toggleNavbar.removeClass('active');				
-			
-			
-		}
-	});
-
-
-});
-
-
