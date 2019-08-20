eb8380d113011ebd3d4906aad494ca8b8de0885c
Nika <n.kublashvili@webintelligence.de>
2019-08-20 23:55:31 +0400

    add animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,10 +11,10 @@ $(document).ready(function($) {
 
         if (!$(this).hasClass('active')) {
             $(this).addClass('active');
-            toggleNavbar.addClass('active');
+            toggleNavbar.addClass('active animated zoomIn');
         } else {
             $(this).removeClass('active');
-            toggleNavbar.removeClass('active');
+            toggleNavbar.removeClass('active zoomIn');
         }
 
     });
@@ -25,12 +25,14 @@ $(document).ready(function($) {
 
         if (!$(this).hasClass('active')) {
             $(this).addClass('active');
-            toggleMenu.addClass('active');
+            toggleMenu.addClass('active animated slideInLeft');
+            toggleBurger.addClass('change');
 
 
         } else {
             $(this).removeClass('active');
-            toggleMenu.removeClass('active');
+            toggleMenu.removeClass('active animated slideInLeft');
+            toggleBurger.removeClass('change');
 
         }
     });
